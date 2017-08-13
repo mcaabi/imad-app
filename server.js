d6dbec38197a726b129146ca6c1f93bcb9b5bdd5
mcaabi <30954736+mcaabi@users.noreply.github.com>
2017-08-13 18:45:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone={
+  title:'Artile one|abi',
+  heading:'Article one',
+  date:'aug 13',
+  content:'
+              <p>
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+               </p>
+               <p>
+                   This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+               </p>'
+           
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
