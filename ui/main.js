1a9fe41a4c20a753c04b322ccb96edd9406dc1a9
mcaabi <30954736+mcaabi@users.noreply.github.com>
2017-08-13 20:08:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
 //change the value
-var element=document.getElementById("text")
-element.innerHTML="new value"
+var element=document.getElementById("text");
+element.innerHTML="new value";
+
+//move the img
+var img=document.getElementById('modi')
+img.onclick=function (){
+  img.style.marginleft='100px';  
+
+};
