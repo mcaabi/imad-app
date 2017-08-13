78b289fbefbe9327d96a4008f22cae099fe5880a
mcaabi <30954736+mcaabi@users.noreply.github.com>
2017-08-13 20:14:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,14 @@ element.innerHTML="new value";
 
 //move the img
 var img=document.getElementById('modi')
+var marginLeft=0;
+function moveRight () {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+    
+}
 img.onclick=function (){
+    var interval=setInterval(moveRight,100);
   img.style.marginleft='100px';  
 
 };
